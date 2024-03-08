0e52859160dcf333206cb95cb4d02df094e6b2fd
eimerqiu <eimerqiu@gmail.com>
2024-03-08 23:31:31 +0800

    fist page

--- tarot_button/lib/main.dart
+++ tarot_button/lib/main.dart
@@ -37,27 +37,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late CameraController _controller;
-  late Future<void> _initializeControllerFuture;
+  CameraController? _controller;
+  Future<void>? _initializeControllerFuture;
+  List<CameraDescription>? _cameras;
+  int _currentCameraIndex = 0;
+
+  // Define the variables for name and question
+  String name = 'Baby';
+  String question = 'Will my dreams come true?';
 
   @override
   void initState() {
     super.initState();
-    _controller = CameraController(
-      widget.camera,
-      ResolutionPreset.medium,
-    );
-    _initializeControllerFuture = _controller.initialize();
+    _initCamera();
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  void _initCamera() async {
+    _cameras = await availableCameras();
+    if (_cameras!.isEmpty) {
+      print('No cameras are available.');
+      return;
+    }
+    _setCurrentCamera(_currentCameraIndex);
+  }
+
+  void _setCurrentCamera(int cameraIndex) async {
+    if (_cameras == null || _cameras!.length <= cameraIndex) {
+      print('Selected camera is not available.');
+      return;
+    }
+
+    final camera = _cameras![cameraIndex];
+    _controller = CameraController(camera, ResolutionPreset.medium);
+    _initializeControllerFuture = _controller?.initialize();
+    await _initializeControllerFuture;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  void _switchCamera() {
+    _currentCameraIndex = (_currentCameraIndex + 1) % _cameras!.length;
+    _setCurrentCamera(_currentCameraIndex);
   }
 
   void _onMeowPressed() {
     // Implement your functionality for the Meow button
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
   }
 
   @override
@@ -66,39 +97,62 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text('Tarot Button'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'User’s Name + Question',
-              style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold), // Enlarged font size
-            ),
-            SizedBox(height: 8),
-            FutureBuilder<void>(
-              future: _initializeControllerFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  // Adjusting size of the camera preview
-                  return Container(
-                    width: 300, // Set your desired width
-                    height: 500, // Set your desired height
-                    child: CameraPreview(_controller),
-                  );
-                } else {
-                  return Center(child: CircularProgressIndicator());
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 8),
+          Text(
+            '$name: $question',
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8),
+          FutureBuilder<void>(
+            future: _initializeControllerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                // Ensure the camera is initialized
+                if (_controller == null || !_controller!.value.isInitialized) {
+                  return Text('Camera is not initialized');
                 }
-              },
-            ),
-            SizedBox(height: 8),
-            FloatingActionButton(
-              onPressed: _onMeowPressed,
-              child: Text('Meow'),
-            ),
-          ],
-        ),
+                return Container(
+                  width: 300,
+                  height: 460,
+                  child: CameraPreview(_controller!),
+                );
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+          SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Meow button - larger and round
+              SizedBox(
+                width: 100, // Increase the width as needed
+                height: 100, // Increase the height to make the button round
+                child: FloatingActionButton(
+                  onPressed: _onMeowPressed,
+                  child: Text('Meow'),
+                  shape: CircleBorder(), // Make the button round
+                ),
+              ),
+              // Space between buttons
+              SizedBox(width: 20),
+              // Smaller switch camera button
+              SizedBox(
+                width: 30, // Smaller width
+                height: 30, // Smaller height
+                child: FloatingActionButton(
+                  onPressed: _switchCamera,
+                  child: Icon(Icons.switch_camera),
+                  mini: true, // Makes the button smaller
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
