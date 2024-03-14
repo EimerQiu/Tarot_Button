0baf2ced4c25a0525177e9efdc832ee8279ceba3
eimerqiu <eimerqiu@gmail.com>
2024-03-14 23:50:57 +0800

    Tarot 2

--- tarot_button/lib/main.dart
+++ tarot_button/lib/main.dart
@@ -93,64 +93,60 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _onMeowPressed() async {
     try {
+      print('_onMeowPressed: Ensuring controller is initialized');
       // Ensure the controller is initialized
       await _initializeControllerFuture;
+      print('_onMeowPressed: Controller initialized');
 
       // Take the picture
       if (_controller != null) {
-        print('_onMeowPressed: Taking a picture...');
+        print('_onMeowPressed: Starting picture-taking process');
         final XFile image = await _controller!.takePicture();
         print('_onMeowPressed: Picture taken: ${image.path}');
 
         // Save the image in the app's directory
         final Directory extDir = await getApplicationDocumentsDirectory();
         final String dirPath = '${extDir.path}/pictures/';
-        await Directory(dirPath).create(recursive: true);
+        var directory = Directory(dirPath);
+        if (await directory.exists()) {
+          print('_onMeowPressed: Directory already exists');
+        } else {
+          print('_onMeowPressed: Creating directory');
+          await directory.create(recursive: true);
+          print('_onMeowPressed: Directory created');
+        }
         final String filePath =
             join(dirPath, '${DateTime.now().millisecondsSinceEpoch}.png');
 
         // Copy the file to a new path
+        print('_onMeowPressed: Copying file to new path');
         final File newImage = await File(image.path).copy(filePath);
         print('_onMeowPressed: Image saved at: $filePath');
 
         // Prepare for uploading the image and other data
         var request = http.MultipartRequest(
-            'POST', Uri.parse('https://aibutton.tech/taro'));
-
-        // Add the image file
+            'POST', Uri.parse('http://192.168.10.65:8000/tarot'));
+        print('_onMeowPressed: Preparing HTTP request');
         request.files
             .add(await http.MultipartFile.fromPath('image', newImage.path));
-
-        // Add other fields
         request.fields['name'] = name;
         request.fields['question'] = question;
 
         // Send the request
+        print('_onMeowPressed: Sending HTTP request');
         var response = await request.send();
+        print(
+            '_onMeowPressed: HTTP request sent, status code: ${response.statusCode}');
 
         // Simulate the response content
         if (response.statusCode == 200) {
-          // original code
-          // if (true) {
-          // Parse the response
           String responseBody = await response.stream.bytesToString();
-
-          // Simulate the response content
-          // responseBody = "Hello, world";
-          // responseBody = "\nHello, $name! \n\n"
-          //     "The image shows the Nine of Pentacles from a tarot deck, which is often associated with abundance, self-sufficiency, and financial stability. The card depicts a figure standing in a garden of grapevines, symbolizing the fruits of one's labor and the rewards of diligence and hard work. The nine pentacles arranged in a lush environment suggest a time of personal achievement and material gain.\n\n"
-          //     "As a fortune teller interpreting this card for your luck in 2024, it would bode well for prosperity and comfort. The card indicates that through continued effort and maintaining a clear vision of your goals, you will likely find yourself in a position of both financial and personal fulfillment.\n"
-          //     "Regarding the question about receiving an offer from Stevenson school this month, the Nine of Pentacles is a positive omen, suggesting that the outcome you are hoping for is within reach. However, remember that tarot readings are not definitive answers but rather reflections of potential outcomes based on current circumstances.\n"
-          //     "Keep putting forth your best efforts, and stay optimistic about the opportunities ahead.\n\n"
-          //     "这张图片显示了塔罗牌上的星币九，它通常与丰富、自给自足和财务稳定联系在一起。这张牌描绘了一个站在葡萄藤花园里的人物，象征着一个人的劳动成果以及勤奋和努力的回报。努力工作。郁郁葱葱的环境中排列的九个星币暗示着个人成就和物质收获的时期。\n\n"
-          //     "作为一名算命师，这张牌为你解读2024年的好运，这将是繁荣和舒适的好兆头。这张牌表明，通过持续努力和保持对目标的清晰愿景，你可能会发现自己处于财务和幸福的境地，和个人成就感。\n\n"
-          //     "关于本月收到史蒂文森学校录取通知书的问题，星币九是一个积极的预兆，表明你所希望的结果是触手可及的。但是，请记住，塔罗牌占卜并不是确定的答案，而是潜力的反映。根据当前情况得出的结果。\n\n"
-          // "继续尽最大努力，并对未来的机会保持乐观。\n\n";
+          print('_onMeowPressed: Response received: $responseBody');
 
           String title = "$name: $question";
           String imagePath = newImage.path;
           String content = responseBody;
-          // Navigate to DisplayPage with the response data
+          print('_onMeowPressed: Navigating to DisplayPage');
           Navigator.of(context).push(MaterialPageRoute(
             builder: (context) => DisplayPage(
               title: title,
@@ -160,13 +156,12 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ));
         } else {
-          // Handle the failure of the upload
-          print('Failed to upload picture.');
+          print(
+              'Failed to upload picture. Status code: ${response.statusCode}');
         }
       }
     } catch (e) {
-      // Handle any exceptions
-      print(e);
+      print('_onMeowPressed: Exception caught: $e');
     }
   }
 
